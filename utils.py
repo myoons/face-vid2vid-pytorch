52cf1b9f7381c70725fe343d83edce232d0f5040
myoons <grandchasevs@kaist.ac.kr>
2021-08-20 12:23:58 +0900

    utils

--- utils.py
+++ utils.py
@@ -23,14 +23,24 @@ def video2png(video_path):
     assert vid_width == 224 and vid_height == 224, print(f"{target_dir} is not 224 * 224")
 
     vid_length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-    for frame_idx in range(0, vid_length, 50):
-        vidcap.set(cv2.CAP_PROP_FRAME_COUNT, frame_idx)
-        ret, image = vidcap.read()
+    if vid_length > 250:
+        for frame_idx in range(0, vid_length, 250):  # 1 frame per 10 seconds
+            vidcap.set(cv2.CAP_PROP_FRAME_COUNT, frame_idx)
+            ret, image = vidcap.read()
 
-        cv2.imwrite(f"{target_dir}/{str(frame).zfill(4)}.jpg", image)
-        if ret is False:
-            break
+            cv2.imwrite(f"{target_dir}/{str(frame).zfill(4)}.png", image)
+            if ret is False:
+                break
+            frame += 1
+    else:
+        vidcap.set(cv2.CAP_PROP_FRAME_COUNT, 0)
+        ret, image = vidcap.read()
+        cv2.imwrite(f"{target_dir}/{str(frame).zfill(4)}.png", image)
         frame += 1
+
+        vidcap.set(cv2.CAP_PROP_FRAME_COUNT, vid_length-1)
+        ret, image = vidcap.read()
+        cv2.imwrite(f"{target_dir}/{str(frame).zfill(4)}.png", image)
 
     print(f"{target_dir} Done")
 
@@ -42,3 +52,22 @@ def mp_video2png(n_processes):
     pool.map(video2png, inputs)
     pool.close()
     pool.join()
+
+
+def resize(image):
+    os.makedirs(image.replace("vox", "vox_256").replace(os.path.basename(image), ""), exist_ok=True)
+    cv2.imwrite(image.replace("vox", "vox_256"), cv2.resize(cv2.imread(image), (256, 256), interpolation=cv2.INTER_CUBIC))
+    print(image.replace("vox", "vox_256"))
+
+
+def mp_resize(n_processes):
+    pool = multiprocessing.Pool(processes=n_processes)
+    inputs = glob("data/vox/**/**/**.png")
+
+    pool.map(resize, inputs)
+    pool.close()
+    pool.join()
+
+
+if __name__ == '__main__':
+    mp_resize(12)
